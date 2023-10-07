0cd121a5292a71ea3339bc30bb0442be17081545
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-10-07 08:41:20 +0530

    added change city name

--- src/Views/Home/Home.js
+++ src/Views/Home/Home.js
@@ -5,18 +5,16 @@ import "./Home.css";
 
 function Home() {
   const [news, setNews] = useState([]);
-  const [searchQuery, setSearchQuery ] = useState(" pune");
+  const [searchQuery, setSearchQuery] = useState("BMW");
 
   const loadNews = async () => {
-    try{
-        const response = await axios.get(
-            
-            `https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-06&to=2023-10-06&sortBy=popularity&apiKey=cd831287c8b74f09a3a4861c8d5306e1`
-          );
-          setNews(response.data.articles);
-    }catch(err)
-    {
-        console.log(err);
+    try {
+      const response = await axios.get(
+        `https://newsapi.org/v2/everything?q=${searchQuery}&from=2023-10-06&to=2023-10-06&sortBy=popularity&apiKey=cd831287c8b74f09a3a4861c8d5306e1`
+      );
+      setNews(response.data.articles);
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -24,18 +22,18 @@ function Home() {
     loadNews();
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     loadNews()
   }, [searchQuery])
   return (
     <div>
       <h1 className="headline">News App</h1>
-      
-     <div className="input-div">
-     <input className="input" type="text" value={searchQuery} onChange={(e)=>{
-        setSearchQuery(e.target.value)
-      }}  />
-     </div>
+
+      <div className="input-div">
+        <input className="input" type="text" value={searchQuery} onChange={(e) => {
+          setSearchQuery(e.target.value)
+        }} />
+      </div>
       <div className="dis-flex">
         {news.map((newsArticals, index) => {
           const {
@@ -47,7 +45,7 @@ function Home() {
             publishedAt,
             content,
           } = newsArticals;
-
+// return value
           return (
             <>
               <NewsArticle
